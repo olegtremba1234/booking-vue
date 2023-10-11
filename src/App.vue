f7150494fb6e49971891a1f240c8d9a975840eef
olegtremba1234 <olegtremba@gmail.com>
2023-10-11 11:41:25 +0300

    add custom input

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script>
 import ApartmentsItem from "./components/apartment/ApartmentsItem.vue";
 import ApartmentsList from "./components/apartment/ApartmentsList.vue";
+import CustomInput from "./components/shared/CustomInput.vue";
 import apartments from "./components/apartment/apartments";
 export default {
   name: "App",
-  components: { ApartmentsList, ApartmentsItem },
+  components: { ApartmentsList, ApartmentsItem, CustomInput },
   data() {
     return {
+      text: "",
       apartments,
     };
   },
@@ -20,6 +22,9 @@ export default {
 
 <template>
   <div class="app">
+    <h2>{{ text }}</h2>
+    <CustomInput v-model="text" />
+    <!-- <input type="text" v-model="text" /> -->
     <ApartmentsList :items="apartments">
       <template v-slot:title>New title</template>
       <template v-slot:apartment="{ apartment }">
---
+++ src/components/shared/CustomInput.vue
@@ -0,0 +1,42 @@
+<template>
+  <input v-bind="$attrs" class="customInput" v-model="inputValue" />
+</template>
+
+<script>
+export default {
+  name: "CustomInput",
+  props: {
+    value: String,
+  },
+  data() {
+    return {
+      inputValue: this.value,
+    };
+  },
+  //   methods: {
+  //     inputChanged(event) {
+  //       this.$emit("input", event.target.value);
+  //     },
+  //   },
+  //   computed: {
+  //     listeners() {
+  //       return {
+  //         ...this.$listeners,
+  //         input: this.inputChanged,
+  //       };
+  //     },
+  //   },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/vars";
+.customInput {
+  min-height: 40px;
+  border: 2px solid $main-color;
+  font-size: 18px;
+  outline: none;
+  line-height: inherit;
+  padding: 8px 15px;
+}
+</style>
